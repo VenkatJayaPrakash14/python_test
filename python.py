adf17c5ac7c3b24cb15d364c9a36ba054c7db990
venkat jaya prakash G <venkatjayaprakash14@gmail.com>
2024-02-13 00:07:15 +0530

    adding code

---
+++ python.py
@@ -0,0 +1,17 @@
+def venky_huku(a,b):
+    for i in a:
+        for j in b:
+            return a[i]
+
+
+a = venky_huku([1,23],[2,34])
+print(a)
+# x=[1,2,3,4,56,6,6]
+
+# sq_fn = lambda x: x * x 
+# vamsi = map(sq_fn,x)
+
+# for i in x:
+#     print(i)
+
+# print(x)
